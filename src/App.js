e72c3f63b74fd4dc915953efcc5a003aaa3b7042
Your Name <you@example.com>
2021-10-26 15:35:34 +0300

    worked created own library using storybook + webpack

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "ellie-component-library/lib/main.css";
+import { Button } from "ellie-component-library";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        styles={{ width: "50px", height: "50px" }}
+        type="download"
+        onClick={() => {
+          console.log("worked created own library using storybook + webpack ");
+        }}
+      >
+        Download
+      </Button>
     </div>
   );
 }
